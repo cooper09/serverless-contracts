b3802f00b5f544182b79fb7e0886ddd0a686592e
cooper09 <cooperatkmba@gmail.com>
2021-09-20 09:22:27 -0400

    simple test function to load data

--- testfunc.js
+++ testfunc.js
@@ -1,13 +1,19 @@
+const axios = require('axios');
+
 const url = 'https://sonyainc.net/json/test.json';
 
 const fetchRate = async () => {
     return new Promise((resolve,reject) => {
-        fetch('https://sonyainc.net/json/test.json')
-        .then(response => response.json())
-        //.then(data => console.log(data));
-        .then(resolve(data));    
-    })
-}
+    try {
+        axios.get('https://sonyainc.net/json/test.json')
+            .then(resp => console.log("response: ", resp.data.string ))  
+        } catch (e) {
+            console.log("Error loading data: ", e );
+        }
+        
+    })//end Promise
+
+}//end fetchRate
 
 const send = async (amount, toAddress) => {
     // 
