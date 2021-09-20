d44f1c18cdb502fafcf39f1c1585483a1b726e3e
cooper09 <cooperatkmba@gmail.com>
2021-09-20 18:03:43 -0400

    latest khalid version

--- handler.js
+++ handler.js
@@ -2,9 +2,14 @@
 
 let exchange = require('./testfunc')
 
-module.exports.get_rate = async event => {
-  let resp = await exchange.fetchRate()
+module.exports.get_all_rates = async event => {
+  let answer = await exchange.fetchAllRates(event)
+  return JSON.stringify(answer)
+}
 
+module.exports.get_rate = async event => {
+  let { exchange, coin} = event;  
+  let resp = await exchange.fetchRate(exchange, coin)
   return {
     statusCode: 200,
     body: JSON.stringify(
@@ -22,24 +27,6 @@ module.exports.get_rate = async event => {
 
 module.exports.send_data = async event => {
   let resp = await exchange.send()
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        test_result: resp,
-      },
-      null,
-      2
-    ),
-  };
-
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
-
-module.exports.get_rate = async event => {
-  let resp = await exchange.fetchRate()
 
   return {
     statusCode: 200,
--- testfunc.js
+++ testfunc.js
@@ -2,16 +2,39 @@ const axios = require('axios');
 
 const url = 'https://sonyainc.net/json/test.json';
 
-const fetchRate = async () => {
-    return new Promise((resolve,reject) => {
-
-    try {
-        axios.get('https://sonyainc.net/json/test.json')
-            .then(resp => console.log("response: ", resp.data.string ))  
-        } catch (e) {
-            console.log("Error loading data: ", e );
+const fetchAllRates = async config => {
+    console.log(`config: ${config}`)
+    return new Promise((resolve, reject )=>{
+        let exchanges = Object.keys(config)
+        let promises = []
+        for( let key in exchanges) {
+            let value = exchanges[key]
+            console.log(`exchange: ${key} => ${value}`)
+            promises.push(fetchRate(key,value))
         }
+        Promise.all(promises).then(results=>{
+            resolve(results)
+        })
         
+    })    
+}
+
+const makeDecision = async rates => {
+    return "decision"
+}
+
+const fetchRate = async (xchg, token) => {
+    //console.log("fetchRate: ", xchg )
+    return new Promise((resolve, reject) => {
+        axios.get(`https://sonyainc.net/json/test.json?exchange=${xchg}&token=${token}`)
+            .then(resp => {
+                console.log("response: ", resp.data)
+                resolve(resp.data)
+            })
+            .catch(e => {
+                console.log("Error loading data: ", e);
+                reject(e)
+            })
     })//end Promise
 
 }//end fetchRate
@@ -27,6 +50,7 @@ const recieve = async (amount, fromAddress) => {
 }
 
 module.exports = {
+    fetchAllRates,
     fetchRate,
     send,
     recieve
